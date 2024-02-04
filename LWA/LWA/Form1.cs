c1ce5a3edb75456345697618e929bfd49aaccb20
Kavindu Maduranga <144511926+gmandroroshe@users.noreply.github.com>
2024-02-04 07:18:01 +0530

    Add files via upload

---
+++ LWA/LWA/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LWA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);//string -- > Integer 
+            int y = int.Parse(textBox2.Text);//string -- > Integer 
+            int ans = (x + y)*2;
+
+            textBox3.Text = ans.ToString();
+            //Integer -- > String 
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);//string -- > Integer 
+            int y = int.Parse(textBox2.Text);//string -- > Integer 
+            int ans = x * y;
+
+            textBox4.Text = ans.ToString();
+            //Integer -- > String 
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
